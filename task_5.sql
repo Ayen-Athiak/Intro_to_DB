0e55339182670933ee51de4041a7ed8fbe562b53
Ayen-Athiak <ayenathiak@gmail.com>
2024-08-12 18:19:11 +0200

    files updated

--- task_5.sql
+++ task_5.sql
@@ -3,5 +3,6 @@ INSERT INTO customers(
     customer_id,customer_name,email,address
 )
 VALUES (
-    1,'John Doe','john.doe@example.com','123 Main St'
+    1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.'
+    
 );
